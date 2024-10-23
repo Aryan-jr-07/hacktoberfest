9d633cba40439f017aebb066b0bd766e2491533d
Aryan <aryan@Aryans-MacBook-Pro-7.local>
2024-10-23 17:56:45 +0530

    Fixed

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-pirnt("Hello World")
+def print_diamond(n):
+    for i in range(n):
+        print(" " * (n - i - 1), end="")
+        print("*" * (2 * i + 1))
+    
+    for i in range(n - 2, -1, -1):
+        print(" " * (n - i - 1), end="")
+        print("*" * (2 * i + 1))
+
+n = int(input())
+print_diamond(n)
